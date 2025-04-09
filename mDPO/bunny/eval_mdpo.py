780d40fe09f1bea74cfb1ed65b2b3cf82876f959
dasgauranga2 <dasgauranga2@gmail.com>
2025-04-09 12:49:05 +0930

    Some changes to evaluation script

--- mDPO/bunny/eval_mdpo.py
+++ mDPO/bunny/eval_mdpo.py
@@ -11,40 +11,48 @@ transformers.logging.disable_progress_bar()
 # warnings.filterwarnings('ignore')
 
 # set device
-device = 'cuda'  # or cpu
+device = 'cuda'
 torch.set_default_device(device)
 
-# create model
+# load the base model
 model = AutoModelForCausalLM.from_pretrained(
     'BAAI/Bunny-v1_0-3B',
     torch_dtype=torch.float16, # float32 for cpu
     device_map='auto',
     trust_remote_code=True)
 
-model = PeftModel.from_pretrained(
-    model,
-    './checkpoint/mdpo_bunny'
-)
+# path of saved checkpoint
+checkpoint_path = './checkpoint/mdpo_bunny'
+# determine if LoRA adapter weights should be used
+use_lora = True
 
-model = model.merge_and_unload()
+if use_lora:
+    model = PeftModel.from_pretrained(
+        model,
+        checkpoint_path
+    )
 
+    model = model.merge_and_unload()
+
+# set model to evaluation mode
 model.eval()
 
+# load the model tokenizer
 tokenizer = AutoTokenizer.from_pretrained(
-    './checkpoint/mdpo_bunny',
+    checkpoint_path,
     trust_remote_code=True)
 
-# text prompt
-prompt = 'What is a vicuna standing in the sand looking at?'
+# prrompt text
+prompt = 'Is the mouse connected to a computer?'
 text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
 text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
 input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
 
-# image, sample images can be found in images folder
-image = Image.open('./data/test1.png')
+# load the image
+image = Image.open('./data/test2.png')
 image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
 
-# generate
+# generate the model outputs
 output_ids = model.generate(
     input_ids,
     images=image_tensor,
@@ -53,4 +61,5 @@ output_ids = model.generate(
     repetition_penalty=1.0 # increase this to avoid chattering
 )[0]
 
+# display the generated text
 print(tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip())
