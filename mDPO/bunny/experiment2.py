528f6b903e6a8a34c180c9e606a835d37e4b5445
dasgauranga2 <dasgauranga2@gmail.com>
2025-04-19 21:14:24 +0000

    Get next token probabilities of partial resppnse

---
+++ mDPO/bunny/experiment2.py
@@ -0,0 +1,216 @@
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+import warnings
+from peft import PeftModel
+from bunny_utils.util.mm_utils import tokenizer_image_token
+from torchvision.transforms import v2
+
+# disable some warnings
+transformers.logging.set_verbosity_error()
+transformers.logging.disable_progress_bar()
+# warnings.filterwarnings('ignore')
+
+# set device
+device = 'cuda'
+torch.set_default_device(device)
+
+# load the base model
+model = AutoModelForCausalLM.from_pretrained(
+    'BAAI/Bunny-v1_0-3B',
+    torch_dtype=torch.float16, # float32 for cpu
+    device_map='auto',
+    trust_remote_code=True)
+
+# load the base model
+model = AutoModelForCausalLM.from_pretrained(
+    'BAAI/Bunny-v1_0-3B',
+    torch_dtype=torch.float16, # float32 for cpu
+    device_map='auto',
+    trust_remote_code=True)
+
+# path of saved checkpoint
+checkpoint_path = './checkpoint/mdpo_bunny'
+# # determine if LoRA adapter weights should be used
+# use_lora = True
+
+# if use_lora:
+#     model = PeftModel.from_pretrained(
+#         model,
+#         checkpoint_path
+#     )
+
+#     model = model.merge_and_unload()
+
+# set model to evaluation mode
+model.eval()
+
+# load the model tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    checkpoint_path,
+    trust_remote_code=True)
+
+# # function to crop an image
+# def crop_image(image):
+#     resize_cropper = v2.RandomResizedCrop(size=image.size()[-2:], scale=(0.01, 0.2))
+#     image = resize_cropper(image.squeeze(0)).unsqueeze(0)
+#     return image
+
+# processes a single data point
+def prepare_inputs(prompt, partial_response, img_path, tokenizer, model):
+    # dictionary to store the inputs
+    batch = {}
+
+    # tokenize the response texts
+    response_tokens = tokenizer(partial_response, add_special_tokens=False)
+    #rejected_tokens = tokenizer(rejected, add_special_tokens=False)
+
+    prompt_tokens = {}
+    # prompt token ids
+    prompt_tokens["input_ids"] = tokenizer_image_token(prompt, tokenizer)
+    # the attention mask helps the model differentiate between actual input tokens and padding tokens
+    prompt_tokens["attention_mask"] = [1 for _ in range(len(prompt_tokens["input_ids"]))]
+
+    # get the end-of-sequence token id
+    eos_token_id = tokenizer.eos_token_id
+
+    # the steps below adjust the attention mask by setting the position corresponding to exisiting EOS tokens to 0
+    # this ensures that the model does not attend to any tokens that come after the EOS token
+    eos_indices_prompt = [i for i, x in enumerate(prompt_tokens["input_ids"]) if x == eos_token_id]
+    # attention mask these indices to eos_token_id
+    new_attention_mask = [
+        0 if i in eos_indices_prompt else p for i, p in enumerate(prompt_tokens["attention_mask"])
+    ]
+    prompt_tokens["attention_mask"] = new_attention_mask
+
+    # do the same for chosen and rejected responses
+    eos_indices_response = [i for i, x in enumerate(response_tokens["input_ids"]) if x == eos_token_id]
+    new_attention_mask_c = [
+        0 if i in eos_indices_response else p for i, p in enumerate(response_tokens["attention_mask"])
+    ]
+    response_tokens["attention_mask"] = new_attention_mask_c
+
+    # # add EOS token to end of responses
+    # response_tokens["input_ids"].append(tokenizer.eos_token_id)
+    # response_tokens["attention_mask"].append(1)
+
+    # rejected_tokens["input_ids"].append(tokenizer.eos_token_id)
+    # rejected_tokens["attention_mask"].append(1)
+
+    # # determine the longer of the chosen and rejected response
+    # longer_response_length = max(len(chosen_tokens["input_ids"]), len(rejected_tokens["input_ids"]))
+
+    # # if combined sequence is too long, truncate the prompt
+    # if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
+    #     if self.truncation_mode == "keep_start":
+    #         prompt_tokens = {k: v[: self.max_prompt_length] for k, v in prompt_tokens.items()}
+    #     elif self.truncation_mode == "keep_end":
+    #         prompt_tokens = {k: v[-self.max_prompt_length :] for k, v in prompt_tokens.items()}
+    #     else:
+    #         raise ValueError(f"Unknown truncation mode: {self.truncation_mode}")
+
+    # # if that's still too long, truncate the response
+    # if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
+    #     chosen_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in chosen_tokens.items()}
+    #     rejected_tokens = {
+    #         k: v[: self.max_length - self.max_prompt_length] for k, v in rejected_tokens.items()
+    #     }
+
+    # concatenate the prompt and response tokens
+    response_sequence_tokens = {k: prompt_tokens[k] + response_tokens[k] for k in response_tokens}
+    #rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
+    # lables are created from the above tokens such that
+    # tokens corresponding to prompt tokens are masked 
+    response_sequence_tokens["labels"] = response_sequence_tokens["input_ids"][:]
+    response_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [-100] * len(
+        prompt_tokens["input_ids"]
+    )
+    # rejected_sequence_tokens["labels"] = rejected_sequence_tokens["input_ids"][:]
+    # rejected_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [-100] * len(
+    #     prompt_tokens["input_ids"]
+    # )
+
+    for k, toks in {
+        "response": response_sequence_tokens,
+        #"rejected": rejected_sequence_tokens,
+        "prompt": prompt_tokens,
+    }.items():
+        for type_key, tokens in toks.items():
+            if type_key == "token_type_ids":
+                continue
+            batch[f"{k}_{type_key}"] = tokens
+
+    #print('./data/merged_images/' + img_path)
+    image = Image.open(img_path)
+    # process the image into a tensor
+    image_tensor = model.process_images([image], model.config).to(dtype=model.dtype)
+    batch["image"] = image_tensor
+
+    # the final result will be of this format
+    #     batch = {
+    #     "chosen_input_ids": [101, 2023, 2003, 1996, 2419, 3430, 1012, 102, 1],  # input token IDs for the prompt + chosen response
+    #     "chosen_attention_mask": [1, 1, 1, 1, 1, 1, 1, 1, 1],  # attention mask
+    #     "chosen_labels": [-100, -100, -100, -100, -100, -100, -100, 2023, 2003, 1996, 2419, 3430, 1012, 102, 1],  # labels for the prompt + chosen response with prompt part masked
+    #     "rejected_input_ids": [101, 2023, 2003, 1996, 3446, 3430, 1012, 102, 1],  # input token IDs for the prompt + rejected response
+    #     "rejected_attention_mask": [1, 1, 1, 1, 1, 1, 1, 1, 1],  # attention mask
+    #     "rejected_labels": [-100, -100, -100, -100, -100, -100, -100, 2023, 2003, 1996, 3446, 3430, 1012, 102, 1],  # labels for the prompt + rejected response with prompt part masked
+    #     "prompt_input_ids": [101, 2023, 2003, 2019, 2742, 3430, 2007, 2019, 999, 1],  # input token IDs for the prompt with image tokens
+    #     "prompt_attention_mask": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # attention mask for the prompt
+    #     "image": <tensor representation of the image>  # image tensor
+    # }
+
+    return batch
+
+# prompt text with <image> token
+prompt = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\nDescribe the image. ASSISTANT:"
+# chosen response text
+response = "The image features an orange"
+# image path
+image_path = './data/test3.png'
+
+# get the inputs for the model
+data = prepare_inputs(prompt, response, image_path, tokenizer, model)
+#print(data)
+
+# function to get the top token probabilities
+def top_token_probs(model, input_ids, attention_mask, image, tokenizer, top=5):
+
+    with torch.no_grad():
+        # feedforward the inputs
+        outputs = model(
+            input_ids=torch.tensor(input_ids, dtype=torch.long).unsqueeze(0),  # add batch dimension
+            attention_mask=torch.tensor(attention_mask, dtype=torch.long).unsqueeze(0),
+            images=image,
+            labels=None,
+            use_cache=False,
+            output_attentions=False,
+            output_hidden_states=False,
+            return_dict=True
+        )
+
+        # get the logits
+        logits = outputs.logits.squeeze()
+
+        # get logits corresponding to the last token
+        last_token_logits = logits[-1,:]
+        
+        # get probabilities
+        probs = torch.softmax(last_token_logits, dim=-1)
+
+        # list to store probabilities of each token
+        probabilities = []
+
+        # get probabilities of each token
+        for i in range(len(probs)):
+            probabilities.append((tokenizer.decode([i], skip_special_tokens=True), probs[i].item()))
+
+        # sort list of probabilities
+        probabilities.sort(key=lambda x:x[1], reverse=True) 
+
+        # print the top probabilities
+        for tok, prob in probabilities[:top]:
+            print(f"{tok} {prob:.4f}")
+
+#top_token_probs(model, data["response_input_ids"], data["response_attention_mask"], data["image"], tokenizer)
+top_token_probs(model, data["response_input_ids"], data["response_attention_mask"], data["image"], tokenizer, 10)
