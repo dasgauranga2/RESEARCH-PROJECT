dd5ec33fcb699b0481811f54ab1aa127d2d91c7a
dasgauranga2 <dasgauranga2@gmail.com>
2025-05-19 19:32:41 +0930

    mDPO Counting Problem Cases

--- mDPO/bunny/mdpo_exp.py
+++ mDPO/bunny/mdpo_exp.py
@@ -42,18 +42,180 @@ tokenizer = AutoTokenizer.from_pretrained(
     checkpoint_path,
     trust_remote_code=True)
 
-# 1. The image contains four traffic lights. With two of them towards the left and the other two towards the right.
-# load the original image
-org_image = Image.open('./data/test/count1.jpg')
-width, height = org_image.size
-# get the left half of the image
-left_half = org_image.crop((0, 0, width // 2, height))
-# get the right half of the image
-right_half = org_image.crop((width // 2, 0, width, height))
+# # 1. The image contains four traffic lights. With two of them towards the left and the other two towards the right.
+# # load the original image
+# org_image = Image.open('./data/test/count1.jpg')
+# width, height = org_image.size
+# # get the left half of the image
+# left_half = org_image.crop((0, 0, width // 2, height))
+# # get the right half of the image
+# right_half = org_image.crop((width // 2, 0, width, height))
 
-for image, image_type in zip([org_image, left_half, right_half], ["Original", "Left-Half", "Right-Half"]):
+# for image, image_type in zip([org_image, left_half, right_half], ["Original", "Left-Half", "Right-Half"]):
+#     # prompt text
+#     prompt = 'How many traffic lights are there in the image?'
+#     text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
+#     text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
+#     input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
+
+#     image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
+
+#     # generate the model outputs
+#     output_ids = model.generate(
+#         input_ids,
+#         images=image_tensor,
+#         max_new_tokens=100,
+#         use_cache=True,
+#         repetition_penalty=1.0 # increase this to avoid chattering
+#     )[0]
+
+#     # display the generated text
+#     output_text = tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+#     print(f"{image_type}: {output_text}")
+
+
+# # 2. The image shows six zebras in a jungle. 
+# # load the original image
+# org_image = Image.open('./data/test/count3.jpg')
+# width, height = org_image.size
+# # divide the image into three halves such that each portion contains two zebras
+# first_half = org_image.crop((0, 0, width // 2.7, height))
+# second_half = org_image.crop((width // 2.7, 0, (2*width) // 2.7, height))
+# third_half = org_image.crop(((2*width) // 2.7, 0, width, height))
+
+# for image, image_type in zip([org_image, first_half, second_half, third_half], ["Original", "First-Half", "Second-Half", "Third-Half"]):
+#     # prompt text
+#     prompt = 'How many zebras are there in the image?'
+#     text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
+#     text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
+#     input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
+
+#     image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
+
+#     # generate the model outputs
+#     output_ids = model.generate(
+#         input_ids,
+#         images=image_tensor,
+#         max_new_tokens=100,
+#         use_cache=True,
+#         repetition_penalty=1.0 # increase this to avoid chattering
+#     )[0]
+
+#     # display the generated text
+#     output_text = tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+#     print(f"{image_type}: {output_text}")
+
+
+# # 3. The image shows three players playing football. 
+# # load the original image
+# org_image = Image.open('./data/test/count5.jpg')
+# width, height = org_image.size
+# # divide the image into the left and right half
+# left_half = org_image.crop((0, 0, (17*width) // 20, height))
+# right_half = org_image.crop(((17*width) // 20, 0, width, (3*height) // 10))
+
+# for image, image_type in zip([org_image, left_half, right_half], ["Original", "Left-Half", "Right-Half"]):
+#     # prompt text
+#     prompt = 'How many people are there in the image?'
+#     text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
+#     text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
+#     input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
+
+#     image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
+
+#     # generate the model outputs
+#     output_ids = model.generate(
+#         input_ids,
+#         images=image_tensor,
+#         max_new_tokens=100,
+#         use_cache=True,
+#         repetition_penalty=1.0 # increase this to avoid chattering
+#     )[0]
+
+#     # display the generated text
+#     output_text = tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+#     print(f"{image_type}: {output_text}")
+
+
+# # 4. The image shows four chairs around a dining table. 
+# # load the original image
+# org_image = Image.open('./data/test/count5.jpg')
+# width, height = org_image.size
+# # divide the image into the left and right half
+# left_half = org_image.crop((0, 0, (2*width) // 5, height))
+# right_half = org_image.crop(((2*width) // 5, 0, width, height))
+
+# for image, image_type in zip([org_image, left_half, right_half], ["Original", "Left-Half", "Right-Half"]):
+#     # prompt text
+#     prompt = 'How many chairs are there in the image?'
+#     text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
+#     text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
+#     input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
+
+#     image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
+
+#     # generate the model outputs
+#     output_ids = model.generate(
+#         input_ids,
+#         images=image_tensor,
+#         max_new_tokens=100,
+#         use_cache=True,
+#         repetition_penalty=1.0 # increase this to avoid chattering
+#     )[0]
+
+#     # display the generated text
+#     output_text = tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+#     print(f"{image_type}: {output_text}")
+
+
+# # 5. The image shows four chairs around a dining table. 
+# # load the original image
+# org_image = Image.open('./data/test/count6.jpg')
+# width, height = org_image.size
+# # divide the image into the left and right half
+# left_half = org_image.crop((0, 0, (11*width) // 20, height))
+# right_half = org_image.crop(((11*width) // 20, 0, width, height))
+
+# for image, image_type in zip([org_image, left_half, right_half], ["Original", "Left-Half", "Right-Half"]):
+#     # prompt text
+#     prompt = 'How many chairs are there in the image?'
+#     text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
+#     text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
+#     input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
+
+#     image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
+
+#     # generate the model outputs
+#     output_ids = model.generate(
+#         input_ids,
+#         images=image_tensor,
+#         max_new_tokens=100,
+#         use_cache=True,
+#         repetition_penalty=1.0 # increase this to avoid chattering
+#     )[0]
+
+#     # display the generated text
+#     output_text = tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+#     print(f"{image_type}: {output_text}")
+
+
+# 6. The image shows three pillows on a sofa. 
+# load the original image
+org_image = Image.open('./data/test/count7.jpg')
+width, height = org_image.size
+# divide the image into three halves each with one pillow
+first_half = org_image.crop((0, 0, (4*width) // 10, height))
+second_half = org_image.crop(((4*width) // 10, 0, (6*width) // 10, height))
+third_half = org_image.crop(((6*width) // 10, 0, width, height))
+
+for image, image_type in zip([org_image, first_half, second_half, third_half], ["Original", "First-Half", "Second-Half", "Third-Half"]):
     # prompt text
-    prompt = 'How many traffic lights are there in the image?'
+    prompt = 'How many pillows are there in the image?'
     text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
     text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
     input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
