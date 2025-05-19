2e4725e89890448653f1d6b319df9673dcbecf02
dasgauranga2 <dasgauranga2@gmail.com>
2025-05-19 16:24:30 +0930

    mDPO Counting Problem left and right halves

---
+++ mDPO/bunny/mdpo_exp.py
@@ -0,0 +1,75 @@
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+import warnings
+from peft import PeftModel
+
+# disable some warnings
+transformers.logging.set_verbosity_error()
+transformers.logging.disable_progress_bar()
+# warnings.filterwarnings('ignore')
+
+# set device
+device = 'cuda'
+torch.set_default_device(device)
+
+# load the base model
+model = AutoModelForCausalLM.from_pretrained(
+    'BAAI/Bunny-v1_0-3B',
+    torch_dtype=torch.float16, # float32 for cpu
+    device_map='auto',
+    trust_remote_code=True)
+
+# path of saved checkpoint
+checkpoint_path = './checkpoint/mdpo_bunny'
+# determine if LoRA adapter weights should be used
+use_lora = True
+
+if use_lora:
+    model = PeftModel.from_pretrained(
+        model,
+        checkpoint_path
+    )
+
+    model = model.merge_and_unload()
+
+# set model to evaluation mode
+model.eval()
+
+# load the model tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    checkpoint_path,
+    trust_remote_code=True)
+
+# 1. The image contains four traffic lights. With two of them towards the left and the other two towards the right.
+# load the original image
+org_image = Image.open('./data/test/count1.jpg')
+width, height = org_image.size
+# get the left half of the image
+left_half = org_image.crop((0, 0, width // 2, height))
+# get the right half of the image
+right_half = org_image.crop((width // 2, 0, width, height))
+
+for image, image_type in zip([org_image, left_half, right_half], ["Original", "Left-Half", "Right-Half"]):
+    # prompt text
+    prompt = 'How many traffic lights are there in the image?'
+    text = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt} ASSISTANT:"
+    text_chunks = [tokenizer(chunk).input_ids for chunk in text.split('<image>')]
+    input_ids = torch.tensor(text_chunks[0] + [-200] + text_chunks[1], dtype=torch.long).unsqueeze(0).to(device)
+
+    image_tensor = model.process_images([image], model.config).to(dtype=model.dtype, device=device)
+
+    # generate the model outputs
+    output_ids = model.generate(
+        input_ids,
+        images=image_tensor,
+        max_new_tokens=100,
+        use_cache=True,
+        repetition_penalty=1.0 # increase this to avoid chattering
+    )[0]
+
+    # display the generated text
+    output_text = tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+    print(f"{image_type}: {output_text}")
