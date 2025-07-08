f967f84f76855cb52773c78f11421ef11493d25f
dasgauranga2 <dasgauranga2@gmail.com>
2025-07-08 14:32:31 +0930

    Apply YOLO image segmentation on multiple images

--- YOLO/yolo.py
+++ YOLO/yolo.py
@@ -5,42 +5,81 @@ from torchvision.transforms.functional import to_tensor
 import torch.nn.functional as F
 from PIL import Image
 import matplotlib.pyplot as plt
+import json
+import random
+
+# set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # load the model
-model = YOLO("YOLO/yolo11x-seg.pt")
+yolo_model = YOLO("YOLO/yolo11x-seg.pt").to(device)
 
-# path of image
-image_path = "mDPO/data/test1.png"
+# function to draw segmentation masks on an image
+def draw_seg_mask(model, image_path):
+    
+    # run inference on image
+    results = model(image_path)
 
-# run inference on image
-results = model(image_path)
+    # load the image
+    image = Image.open(image_path).convert("RGB")
+    image_tensor = to_tensor(image).mul(255).to(torch.uint8)  # shape: (C, H, W)
 
-# load the image
-image = Image.open(image_path).convert("RGB")
-image_tensor = to_tensor(image).mul(255).to(torch.uint8)  # shape: (C, H, W)
+    # get the segmentation masks
+    masks = results[0].masks.data # shape: (N, H, W)
 
-# get the segmentation masks
-masks = results[0].masks.data # shape: (N, H, W)
+    # dimensions of image
+    _, H, W = image_tensor.shape
 
-# dimensions of image
-_, H, W = image_tensor.shape
+    # scale the masks to original scale
+    masks = F.interpolate(
+        masks.unsqueeze(1),
+        size=(H,W),
+        mode='nearest'
+    ).squeeze().bool()
 
-# scale the masks to original scale
-masks = F.interpolate(
-    masks.unsqueeze(1),
-    size=(H,W),
-    mode='nearest'
-).squeeze().bool()
+    # draw the segmentation masks
+    image_with_masks = draw_segmentation_masks(image_tensor.cpu(),
+                                            masks,
+                                            alpha=0.5,
+                                            colors=['blue']*masks.shape[0])
+    return image_with_masks
 
-# draw the segmentation masks
-image_with_masks = draw_segmentation_masks(image_tensor.cpu(),
-                                           masks,
-                                           alpha=0.5,
-                                           colors=['blue']*masks.shape[0])
+# open the training data json file
+with open('mDPO/data/vlfeedback_llava_10k.json', 'r') as file:
+    data = json.load(file)
 
-# save the image
-plt.figure(figsize=(10, 10))
-plt.imshow(image_with_masks.permute(1, 2, 0))  # convert to (H, W, C)
-plt.axis("off")
-plt.title("YOLOv11 Instance Segmentation")
-plt.savefig(f'YOLO/segment.png', bbox_inches='tight', pad_inches=0, dpi=300)
+# randomly select some images
+image_paths = []
+for sample in random.sample(data, 6):
+    image_paths.append('mDPO/data/merged_images/' + sample['img_path'])
+
+# open the images
+images = []
+for image_path in image_paths:
+    images.append(Image.open(image_path).convert("RGB"))
+
+# draw segmentation masks on the images
+corrupted_images = []
+for image_path in image_paths:
+    corrupted_image = draw_seg_mask(yolo_model, image_path)
+    corrupted_images.append(corrupted_image)
+
+# figure for the original image and the corrupted images
+fig, axes = plt.subplots(len(images), 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i in range(len(images)):
+
+    # plot the original image
+    axes[(i*2)].imshow(images[i])
+    axes[(i*2)].set_title("Original Image")
+    axes[(i*2)].axis('off')
+
+    # plot the corrupted image
+    axes[(i*2)+1].imshow(corrupted_images[i].permute(1, 2, 0))
+    axes[(i*2)+1].set_title(f"Custom Corruption")
+    axes[(i*2)+1].axis('off')
+
+# save the images
+plt.savefig(f'mDPO/results/img_seg_corruption.png', bbox_inches='tight', pad_inches=0, dpi=300)
 plt.close()
