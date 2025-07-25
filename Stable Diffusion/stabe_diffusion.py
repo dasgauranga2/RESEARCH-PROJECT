9c0c0e0a458b728301d5f72eede5cbf932c43b53
dasgauranga2 <dasgauranga2@gmail.com>
2025-07-26 06:14:20 +0930

    Use image generation on preference data to create images

--- Stable Diffusion/stabe_diffusion.py
+++ Stable Diffusion/stabe_diffusion.py
@@ -1,14 +1,23 @@
 import torch
 from diffusers import StableDiffusion3Pipeline
+import json
 
+# load the stable diffusion model
 pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
+# open the training data json file
+with open('mDPO/data/vlfeedback_llava_10k.json', 'r') as file:
+    data = json.load(file)
+
+print(data[0])
+
+# generate the image
 image = pipe(
-    "A cat holding a sign that says hello world",
-    negative_prompt="",
-    num_inference_steps=28,
-    guidance_scale=7.0,
+    data[0]['chosen'], # text prompt for generation
+    negative_prompt=data[0]['rejected'], # negative prompt for undesirable features
+    num_inference_steps=28, # no. of denoising steps for finder details
+    guidance_scale=7.0, # strength of prompt adherence 
 ).images[0]
 
 image.save('Stable Diffusion/test.png')
