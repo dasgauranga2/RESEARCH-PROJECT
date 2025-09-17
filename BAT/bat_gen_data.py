dc3213b7fad4ffc158473eaca8715ada7f9b64ac
dasgauranga2 <dasgauranga2@gmail.com>
2025-09-18 08:42:41 +0930

    Generate data for background association test for multiple background images

--- BAT/bat_gen_data.py
+++ BAT/bat_gen_data.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
 import torch
-from diffusers import StableDiffusion3Pipeline
 import json
 import random
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
 from tqdm import tqdm
-from openai import OpenAI, RateLimitError, InternalServerError
+#from openai import OpenAI, RateLimitError, InternalServerError
 import os
 from torchvision import transforms
 import numpy as np
@@ -15,13 +14,6 @@ import torch.nn.functional as F
 
 # set device
 device = torch.device("cuda")
-
-# # get the OpenAI API key
-# with open("mDPO/MMHal-Bench/api.txt", "r") as f:
-#     API_KEY = f.read().strip()
-
-# # openai client
-# openai_client = OpenAI(api_key=API_KEY)
 
 # load object detection model
 model = YOLO("BAT/yolo11x-seg.pt").to(device)  # load an official model
@@ -40,14 +32,44 @@ for filename in os.listdir('BAT/bg_images/'):
     except:
         print(f"Could not load image at {filepath}")
 
+# function to place segmented objects on background image
+def place_objects(obj_masks, orig_image, height, width, background):
+    # dimensions of segmentation masks
+    h, w = obj_masks.shape[-2], obj_masks.shape[-1]
+    if (h, w) != (height, width):
+        obj_masks = F.interpolate(obj_masks.unsqueeze(1), size=(height, width), mode="nearest").squeeze(1)  # [N,H,W]
+    
+    # build union of masks
+    union_mask = (obj_masks > 0.5).any(dim=0)  # bool [H,W]
+    # convert to PIL mask
+    pil_mask = Image.fromarray((union_mask.numpy().astype(np.uint8) * 255), mode="L")
+    pil_mask = pil_mask.filter(ImageFilter.GaussianBlur(radius=2.0))
+    
+    # resize background image
+    background_resized = background.resize((width, height), Image.BICUBIC)
+
+    # put segemented objects in background image
+    edited_image = Image.composite(
+        orig_image,
+        background_resized,
+        pil_mask
+    )
+
+    return edited_image
+
 # list of original images
 orig_images = []
-# list of images with objects placed on background
+# list of images with objects placed on different background
 edited_images = []
 
-for sample in random.sample(data, 6):
+# list to save the data
+save_data = []
+
+for sample in random.sample(data, 100):
+    # image file name
+    image_name = sample['img_path']
     # image path
-    image_path = 'mDPO/data/merged_images/' + sample['img_path']
+    image_path = 'mDPO/data/merged_images/' + image_name
     # open the image 
     image = Image.open(image_path).convert("RGB")
     # dimensions of image
@@ -59,7 +81,7 @@ for sample in random.sample(data, 6):
         #conf=CONF,
         #iou=IOU,
         #imgsz=IMGSZ,
-        #verbose=False
+        verbose=False
     )[0]
 
     if result.masks is None or result.masks.data is None:
@@ -69,45 +91,55 @@ for sample in random.sample(data, 6):
         # shape: [num_objects, height, width]
         seg_masks = result.masks.data.float().detach().cpu()
 
-        # dimensions of segmentation masks
-        h, w = seg_masks.shape[-2], seg_masks.shape[-1]
-        if (h, w) != (H, W):
-            seg_masks = F.interpolate(seg_masks.unsqueeze(1), size=(H, W), mode="nearest").squeeze(1)  # [N,H,W]
-        
-        # build union of masks
-        union_mask = (seg_masks > 0.5).any(dim=0)  # bool [H,W]
-        # convert to PIL mask
-        pil_mask = Image.fromarray((union_mask.numpy().astype(np.uint8) * 255), mode="L")
-        pil_mask = pil_mask.filter(ImageFilter.GaussianBlur(radius=2.0))
-        
-        # resize background image
-        bg_image_rs = bg_images[0].resize((W, H), Image.BICUBIC)
+        for i, bg_image in enumerate(bg_images):
+            # put segmented objects on background image
+            db_image = place_objects(seg_masks, image, H, W, bg_image)
 
-        # put segemented objects in background image
-        seg_image = Image.composite(
-            image,
-            bg_image_rs,
-            pil_mask
-        )
+            # separate image name and image format
+            splits = image_name.split('.')
+            # new image file name
+            new_image_name = splits[0] + f'_{i}' + '.' + splits[1]
+            
+            #print(new_image_name)
+            db_image.save('BAT/eval_images/' + new_image_name)
 
-        orig_images.append(image)
-        edited_images.append(seg_image)
+        # confidence scores
+        conf = result.boxes.conf.detach()
+        # index of most confident score
+        im = int(conf.argmax())
+        # class id of object category with most confident score
+        cls_id = int(result.boxes.cls[im].item())
+        # class name of object category with most confident score
+        cls_name = result.names[cls_id]
+        #oriscore = float(conf[im].item())
+        #print(conf, im, cls_id, cls_name, score)
 
-# figure for the original image and the edited images
-fig, axes = plt.subplots(len(orig_images), 2, figsize=(8, 3*len(orig_images)))
-axes = axes.flatten()
+        original = sample.copy()
+        original['most_conf_class'] = cls_name
+        save_data.append(original)
 
-for i in range(len(orig_images)):
-    # plot the original image
-    axes[(i*2)].imshow(orig_images[i])
-    axes[(i*2)].set_title("Original Image")
-    axes[(i*2)].axis('off')
+# save the generated data in a json file
+with open('./BAT/bat_data.json', 'w') as f:
+    json.dump(save_data, f, indent=4)
 
-    # plot the chosen image
-    axes[(i*2)+1].imshow(edited_images[i])
-    axes[(i*2)+1].set_title("Edited Image")
-    axes[(i*2)+1].axis('off')
+        # orig_images.append(image)
+        # edited_images.append(seg_image)
 
-# save the images
-plt.savefig(f'BAT/bat_test_images.png', bbox_inches='tight', pad_inches=0, dpi=300)
-plt.close()
+# # figure for the original image and the edited images
+# fig, axes = plt.subplots(len(orig_images), 2, figsize=(8, 3*len(orig_images)))
+# axes = axes.flatten()
+
+# for i in range(len(orig_images)):
+#     # plot the original image
+#     axes[(i*2)].imshow(orig_images[i])
+#     axes[(i*2)].set_title("Original Image")
+#     axes[(i*2)].axis('off')
+
+#     # plot the chosen image
+#     axes[(i*2)+1].imshow(edited_images[i])
+#     axes[(i*2)+1].set_title("Edited Image")
+#     axes[(i*2)+1].axis('off')
+
+# # save the images
+# plt.savefig(f'BAT/bat_test_images.png', bbox_inches='tight', pad_inches=0, dpi=300)
+# plt.close()
